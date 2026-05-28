184db3056a7950755532128e565cf81837eb6a68
SebbyLam <159206469+SebbyLam@users.noreply.github.com>
2026-05-28 04:08:10 +0000

    Changes to v06

--- project/py_scripts/v06.py
+++ project/py_scripts/v06.py
@@ -1,7 +1,7 @@
 from lib.audio_notification import Audio_Notification
 from time import sleep
 
-buzzer = Audio_Notification(18, debug=True)
+buzzer = Audio_Notification(27, debug=True)
 
 print("Testing beep()")
 buzzer.beep(freq=100, duration=200)
@@ -10,11 +10,11 @@ print("Did you hear a beep?(Check your buzzer)")
 print("Testing warning_on() (should beep every -0.5s for 2 seconds)")
 start = buzzer.__last_toggle_time
 for _ in range(5):
-    buzzer.warning_on
+    buzzer.warning_on()
     sleep(0.5)
 
 print("Testing warning_off() (should silence the buzzer)")
-buzzer.warning_off
+buzzer.warning_off()
 print("Buzzer should now be off.")
 
 print("Manual test complete.")
